7d5e634479ef9e47226b689d077b26706d58a350
Pedro Ch <pedro22enriqu@gmail.com>
2022-12-04 13:22:07 -0400

    tostada

--- resources/views/proyectos/usuarios.blade.php
+++ resources/views/proyectos/usuarios.blade.php
@@ -252,13 +252,22 @@
     </div>
 
     @push('scripts')
-        {{-- <script>
-            function invitar() {
-                $.ajax({
-
-                });
-            }
-        </script> --}}
+    <script>
+        @if (Session::has('message'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true,
+            };
+            toastr.success("{{ session('message') }}");
+        @endif
+        @if (Session::has('error'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true,
+            };
+            toastr.error("{{ session('error') }}");
+        @endif
+    </script>
     @endpush
 
 </x-app-layout>
